ee18eeacb62598930a9d46f739c7d015aeb24c00
DisRachik <radchenkoig@gmail.com>
2023-02-20 14:42:04 +0200

    the first task is completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const placeImageInGallery = function (images) {
+	return images
+		.map(
+			({ preview, original, description }) =>
+				`<div class="gallery__item">
+          <a class='gallery__link' href='${original}'>
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+          </a>
+        </div>`,
+		)
+		.join('');
+};
+
+const onImageClick = function (evt) {
+	evt.preventDefault();
+
+	if (evt.target.nodeName !== 'IMG') {
+		return;
+	}
+	showLargeImage(evt.target.dataset.source);
+};
+
+const showLargeImage = function (linkImg) {
+	const instance = basicLightbox.create(`<img src='${linkImg}'>`);
+
+	instance.show();
+
+	const closeLargeImageOnKeyboard = function (evt) {
+		if (evt.code === 'Escape') {
+			document.removeEventListener('keydown', closeLargeImageOnKeyboard);
+			instance.close();
+		}
+	};
+
+	document.addEventListener('keydown', closeLargeImageOnKeyboard);
+};
+
+galleryEl.innerHTML = placeImageInGallery(galleryItems);
+galleryEl.addEventListener('click', onImageClick);
