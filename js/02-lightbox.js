8ee45178870e626d563d841ae97466aa59db63de
DisRachik <radchenkoig@gmail.com>
2023-02-20 16:04:46 +0200

    the second task is completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = placeImageInGallery(galleryItems);
+
+function placeImageInGallery(images) {
+	return images
+		.map(
+			({ preview, original, description }) =>
+				`<a class='gallery__item' href='${original}'>
+            <img class="gallery__image" src="${preview}" alt="${description}">
+          </a>`,
+		)
+		.join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
